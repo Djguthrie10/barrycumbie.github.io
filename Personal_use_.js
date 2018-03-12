b5a81c6fa11b7aff7fd6a818cc05c8339836715c
Djguthrie10 <37086947+Djguthrie10@users.noreply.github.com>
2018-03-12 16:03:41 -0500

    Create Personal_use_.js

---
+++ Personal_use_.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+// create a loop to "print out" all elements on the DOM"
+//1. create a loop structure. How to figure out the Length, i.e.,the number of iterations of my loop
+//2. how do i access an element from my page...then ALL elements?
+//3. how do i want to print them out? Start on the console.
+
+
+var allElem = document.getElementsByTagName("*");
+for (var i = 0; i < allElem.length; i++) {	
+    // Do something with all element here
+	console.log(allElem[i].tagName); //all[i] ... what METHOD do i need.... allElem[i].
+}
